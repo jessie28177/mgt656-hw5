29ee9a586755c09979c888f24924ab34f9db4578
Jessie <jessie.zhao@yale.edu>
2016-10-07 00:54:33 +0000

    Add nickname route

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-    response.send('Hello MGT-656!');
+    response.send('Hello MGT-656! I am Jessie');
+});
+
+app.get('/nickname', function(request, response){
+    response.send('amused-wolf');
 });
 
 app.listen(process.env.PORT || 4000);
